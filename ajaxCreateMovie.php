4ba23d3f0f20ba1f8f1c3bfe379a55a313954d80
agusti <agusti.matamoros20@gmail.com>
2022-12-11 21:35:53 +0100

    Movies can be added without image

--- ajaxCreateMovie.php
+++ ajaxCreateMovie.php
@@ -11,7 +11,7 @@
     $actors = $_POST["actors"];
     $categories = $_POST["categories"];
 
-    $insertMovie = $peliculesdb -> insertOne(['titol' => $titol, 'director' => $nomDirector, 'durada' => $duradaPelicula, 'pegi' => $PEGI, 'valoracio' => $valoracioPelicula, 'any' => $anyPelicula, 'descripcio' => $descripcioPelicula, 'imatge' => $imatgePelicula, 'copies' => 1]);
+    $insertMovie = $peliculesdb -> insertOne(['titol' => $titol, 'director' => $nomDirector, 'durada' => $duradaPelicula, 'pegi' => $PEGI, 'valoracio' => $valoracioPelicula, 'any' => $anyPelicula, 'descripcio' => $descripcioPelicula, 'imatge' => $imatgePelicula]);
 
     $idMovie = (string)$insertMovie->getInsertedId();
     foreach($actors as $a){
@@ -22,5 +22,7 @@
         $updateMovie = $peliculesdb->updateOne(['_id' => new MongoDB\BSON\ObjectId($idMovie)],['$push' => ['categories' => $c]] );
         $updateCategoria = $peliculesdb->updateOne(['_id' => new MongoDB\BSON\ObjectId($c)],['$push' => ['pelicules' => $idMovie]] );
     }
-
+    $insertCopia = $copiesdb->insertOne(['estat' => 0,'pelicula' => $idMovie]);
+    $idCopia = (string)$insertCopia->getInsertedId();
+    $updateMovie = $peliculesdb->updateOne(['_id' => new MongoDB\BSON\ObjectId($idMovie)],['$push' => ['copies' => $idCopia]]);
 ?>
