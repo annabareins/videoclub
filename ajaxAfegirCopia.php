179f2280673e1b3e1a0ff2f509612de8ca3239ab
agusti <agusti.matamoros20@gmail.com>
2022-12-12 17:21:14 +0100

    Copies can be updated

--- ajaxAfegirCopia.php
+++ ajaxAfegirCopia.php
@@ -1,8 +1,11 @@
 <?php
     require_once("connect_data.php");
     $idMovie = $_POST["idPelicula"];
+    $nombreCopies = $_POST["nombreCopies"];
 
-    $insertCopia = $copiesdb->insertOne(['estat' => 0,'pelicula' => $idMovie]);
-    $idCopia = (string)$insertCopia->getInsertedId();
-    $updateMovie =  $updateMovie = $peliculesdb->updateOne(['_id' => new MongoDB\BSON\ObjectId($idMovie)],['$push' => ['copies' => $idCopia]]);
+    for ($i=0;$i<$nombreCopies;$i++){
+        $insertCopia = $copiesdb->insertOne(['estat' => 0,'pelicula' => $idMovie]);
+        $idCopia = (string)$insertCopia->getInsertedId();
+        $updateMovie =  $updateMovie = $peliculesdb->updateOne(['_id' => new MongoDB\BSON\ObjectId($idMovie)],['$push' => ['copies' => $idCopia]]);
+    }
 ?>
