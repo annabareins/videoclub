e65c3e531df7184099936cbe8117a5bd408a6213
agusti <agusti.matamoros20@gmail.com>
2022-12-14 17:39:04 +0100

    FINAL CHANGES

--- menu.php
+++ menu.php
@@ -4,6 +4,7 @@
     <ul>
         <li><a href="index.php"><i class="fa fa-home" aria-hidden="true" title="MENÚ"></i></a></li>
         <li><a href="createMovie.php"><i class="fa fa-plus" aria-hidden="true" title="INSERIR PEL·LICULES"></i></a></li>
+        <li><a href="insertActor.php"><i class="fa fa-video-camera" aria-hidden="true" title="INSERIR DIRECTOR/ACTOR"></i></a></li>
         <li><a href="llistatReserves.php"><i class="fa fa-list-ul" aria-hidden="true" title="RESERVES"></i></a></li>
         <li><a href="pelicules.php"><i class="fa fa-film" aria-hidden="true" title="PELÍCULES"></i></a></li>
         <li><a href="clients.php"><i class="fa fa-user" aria-hidden="true" title="CLIENTS"></i></a></li>
